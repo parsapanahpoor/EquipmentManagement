3221f0f59a0a6918a59e11d9946ae344459800f1
parsapanahpoor <parsapanahpoor77@gmail.com>
2024-01-31 16:21:17 +0330

    Add Architecture

---
+++ EquipmentManagement/EquipmentManagement.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EquipmentManagement.Presentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
