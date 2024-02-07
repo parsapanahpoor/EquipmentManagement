e6e0ebb1ce00878d6c930b600dc3c4285142fac9
parsapanahpoor <parsapanahpoor77@gmail.com>
2024-02-07 13:11:50 +0330

    before add ProductCategory table

---
+++ EquipmentManagement.Domain/Entities/ProductCategory/ProductCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace EquipmentManagement.Domain.Entities.ProductCategory;
+
+public sealed class ProductCategory : BaseEntities<ulong>
+{
+    #region properties
+
+    public ulong BusinessKey { get; set; }
+
+    public string CategoryTitle { get; set; }
+
+    #endregion
+}
