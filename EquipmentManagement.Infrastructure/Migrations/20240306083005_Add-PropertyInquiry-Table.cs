c6bb68ce30b825509a84d7e851ebdcfd5950a878
parsapanahpoor <parsapanahpoor77@gmail.com>
2024-03-06 12:04:03 +0330

    after add property inquiry

---
+++ EquipmentManagement.Infrastructure/Migrations/20240306083005_Add-PropertyInquiry-Table.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EquipmentManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropertyInquiryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OperatorExcelUploadLoggers",
+                columns: table => new
+                {
+                    Id = table.Column<decimal>(type: "decimal(20,0)", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<decimal>(type: "decimal(20,0)", nullable: false),
+                    ExcelFile = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OperatorExcelUploadLoggers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PropertyInquiries",
+                columns: table => new
+                {
+                    Id = table.Column<decimal>(type: "decimal(20,0)", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<decimal>(type: "decimal(20,0)", nullable: false),
+                    ExcelFile = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyInquiries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PropertyInquiryDetails",
+                columns: table => new
+                {
+                    Id = table.Column<decimal>(type: "decimal(20,0)", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RF_Id = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyInquiryDetails", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OperatorExcelUploadLoggers");
+
+            migrationBuilder.DropTable(
+                name: "PropertyInquiries");
+
+            migrationBuilder.DropTable(
+                name: "PropertyInquiryDetails");
+        }
+    }
+}
