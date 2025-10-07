6b2e90c114873820697b8d338c1e50abb976267b
parsapanahpoor <parsapanahpoor77@gmail.com>
2025-10-07 23:13:34 +0330

    Add product creation and deletion commands
    Introduce `CreateProductCommand` and `CreateProductCommandHandler` for managing product creation with validation checks. Add `DeleteProductCommand` and `DeleteProductCommandHandler` for product deletion functionality. Update `Landing.cshtml` for improved UI layout regarding repair and abolition requests. Include new binary image files (`02.jpg` and `03.jpg`).

---
+++ EquipmentManagement.Application/CQRS/SiteSide/Product/Command/CreateProductCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace EquipmentManagement.Application.CQRS.SiteSide.Product.Command;
+
+public class CreateProductCommand : IRequest<bool>
+{
+    #region properties
+
+    public ulong? PlaceId { get; set; }
+
+    public ulong? CategoryId { get; set; }
+
+    public string ProductTitle { get; set; }
+
+    public string BarCode { get; set; }
+
+    public int EntityCount { get; set; }
+
+    public string? Description { get; set; }
+
+    #endregion
+}
