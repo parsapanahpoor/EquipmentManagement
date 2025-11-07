c63b0c78f6060dc19ac0caaf4b264a910d1b1fd4
abolfazl Shabani <57566177+abolfazlshs80@users.noreply.github.com>
2025-11-07 22:58:46 +0330

    update user extension

---
+++ EquipmentManagement.Application/Utilities/Extentions/BoolExtensions.cs
@@ -0,0 +1,13 @@
+﻿using EquipmentManagement.Application.StaticTools;
+using EquipmentManagement.Domain.Entities.Users;
+using System.Security.Claims;
+using System.Security.Principal;
+public static class BoolExtensions
+{
+   
+    public static string ConvertToPersian(this bool? user)
+    {
+        return user.HasValue ? user.Value ? "دارد" : "ندارد" : "ندارد";
+    }
+}
+
--- EquipmentManagement.Application/Utilities/Extentions/UserExtensions.cs
+++ EquipmentManagement.Application/Utilities/Extentions/UserExtensions.cs
@@ -42,3 +42,4 @@ namespace EquipmentManagement.Application.Extensions
         }
     }
 }
+
