a7ab68246cf44f07f17e0fa61ef9768477a827eb
abolfazl Shabani <57566177+abolfazlshs80@users.noreply.github.com>
2025-12-05 13:07:21 +0330

    ✨ feat(employee): implement create employee shift meal selection
    - add command and handler for creating employee shift meal selections
    - include validation to ensure employee shift exists
    - persist employee shift meal selection data to the database

---
+++ EquipmentManagement.Application/CQRS/SiteSide/EmployeeShiftMeals/Command/CreateEmployeeShiftMealSelectedCommand.cs
@@ -0,0 +1,12 @@
+﻿using EquipmentManagement.Domain.Entities.MealPricing;
+
+namespace EquipmentManagement.Application.CQRS.SiteSide.EmployeeShiftMealSelected.Command;
+
+public class CreateEmployeeShiftMealSelectedCommand : IRequest<bool>
+{
+    #region properties
+    public ulong MealPricingId { get; set; }
+    public ulong EmployeeShiftSelectedId { get; set; }
+
+    #endregion
+}
---
+++ EquipmentManagement.Application/CQRS/SiteSide/EmployeeShiftMeals/Command/CreateEmployeeShiftMealSelectedCommandHandler.cs
@@ -0,0 +1,66 @@
+﻿
+using EquipmentManagement.Application.Common.IUnitOfWork;
+using EquipmentManagement.Domain.IRepositories.Employee;
+using EquipmentManagement.Domain.IRepositories.EmployeeShiftMeals;
+using EquipmentManagement.Domain.IRepositories.EmployeeShifts;
+using EquipmentManagement.Domain.IRepositories.MealPricing;
+using Microsoft.EntityFrameworkCore;
+
+namespace EquipmentManagement.Application.CQRS.SiteSide.EmployeeShiftMealSelected.Command;
+
+public record CreateEmployeeShiftMealSelectedCommandHandler : IRequestHandler<CreateEmployeeShiftMealSelectedCommand, bool>
+{
+    #region Ctor 
+
+    private readonly IEmployeeShiftMealSelectedCommandRepository _EmployeeShiftMealSelectedCommandRepository;
+    private readonly IEmployeeShiftMealSelectedQueryRepository _EmployeeShiftMealSelectedQueryRepository;
+    private readonly IEmployeeShiftSelectedQueryRepository _EmployeeShiftQueryRepository;
+    private readonly IMealPricingQueryRepository _MealPricingQueryRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateEmployeeShiftMealSelectedCommandHandler(IEmployeeShiftMealSelectedCommandRepository EmployeeShiftMealSelectedCommandRepository,
+                                    IEmployeeShiftMealSelectedQueryRepository EmployeeShiftMealSelectedQueryRepository, IEmployeeShiftSelectedQueryRepository EmployeeShiftQueryRepository,
+                                    IUnitOfWork unitOfWork, IMealPricingQueryRepository mealPricingQueryRepository)
+    {
+        _EmployeeShiftMealSelectedCommandRepository = EmployeeShiftMealSelectedCommandRepository;
+        _EmployeeShiftMealSelectedQueryRepository = EmployeeShiftMealSelectedQueryRepository;
+        _EmployeeShiftQueryRepository = EmployeeShiftQueryRepository;
+        _unitOfWork = unitOfWork;
+        _MealPricingQueryRepository = mealPricingQueryRepository;
+    }
+
+    #endregion
+
+    public async Task<bool> Handle(CreateEmployeeShiftMealSelectedCommand request, CancellationToken cancellationToken)
+    {
+        #region Check For Title Comming Being Unique
+
+        if (!await _EmployeeShiftQueryRepository.IsExistAnyByIdAsync(request.EmployeeShiftSelectedId, cancellationToken))
+        {
+            return false;
+        }
+
+        #endregion
+
+        #region Add EmployeeShiftMealSelected To The Data Base
+
+        EquipmentManagement.Domain.Entities.Employee.EmployeeShiftMealSelected EmployeeShiftMealSelected = new EquipmentManagement.Domain.Entities.Employee.EmployeeShiftMealSelected()
+        {
+            EmployeeShiftSelectedId = request.EmployeeShiftSelectedId,
+            MealPricingId = request.MealPricingId,
+            MealPricing = await _MealPricingQueryRepository.GetByIdAsync(cancellationToken, request.MealPricingId),
+            EmployeeShiftSelected = await _EmployeeShiftQueryRepository.GetByIdAsync(cancellationToken, request.EmployeeShiftSelectedId)
+        };
+
+        await _EmployeeShiftMealSelectedCommandRepository.AddAsync(EmployeeShiftMealSelected, cancellationToken);
+        await _unitOfWork.SaveChangesAsync();
+
+
+
+        await _unitOfWork.SaveChangesAsync();
+
+        #endregion
+
+        return true;
+    }
+}
