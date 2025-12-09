860d0fd5d5cfc846c43a0b4d83d0fe11ea82b0a0
abolfazl Shabani <57566177+abolfazlshs80@users.noreply.github.com>
2025-12-09 22:39:37 +0330

    ✨ feat(employeeShift): restore VerifyByEmployeeAsync method in IEmployeeShiftSelectedQueryRepository

--- EquipmentManagement.Domain/IRepositories/EmployeeShifts/IEmployeeShiftSelectedQueryRepository.cs
+++ EquipmentManagement.Domain/IRepositories/EmployeeShifts/IEmployeeShiftSelectedQueryRepository.cs
@@ -5,5 +5,6 @@ namespace EquipmentManagement.Domain.IRepositories.EmployeeShifts;
 
 public interface IEmployeeShiftSelectedQueryRepository: IQueryGenericRepository<EmployeeShiftSelected>
 {
- 
+    Task<bool> VerifyByEmployeeAsync( ulong EmployeeId,CancellationToken ct);
+    //Task<ulong> GetEmployeeShiftIdByEmployeeIdAsync(ulong EmployeeId, CancellationToken ct);
 }
